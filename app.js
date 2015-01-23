de8455d5398293ff0bf0295044e3bdc2ab5f5105
Laura Maddox <ljgmaddox@gmail.com>
2015-01-23 15:21:56 -0800

    Simple API request rendered in index view

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+// Set API key from local configuration file
+app.locals.config = require('./config/local_config.json');
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var url = 'https://openapi.etsy.com/v2/listings/active?limit=5&tags=feminine dog collar&fields=title,price,tags,views,num_favorers&sort_on=score&api_key=' + req.app.locals.config.api_key;
+  
+  request({url: url, json: true}, function(error, response, body) { 
+    if (error) { throw error; }
+    res.render('index', { etsy_data: JSON.stringify(body) });
+  });
+
 });
 
 module.exports = router;
