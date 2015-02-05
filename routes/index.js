86b7f004fe304c404764c5f89f5328c06d2b51d4
Laura Maddox <ljgmaddox@gmail.com>
2015-02-05 15:20:27 -0800

    Redirect index POST search to dashboard url, escape punctuation in search

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,24 @@ var helpers = require('../lib/utils');
 var Results = require('../lib/results');
 
 
-/* POST tag search to index route, render dashboard view */
+/* GET index view */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* POST tag search to index route, redirect to dashboard route */
 router.post('/', function(req, res, next) {
   var search_term = req.body.search_tag
+    .replace(/[\.,\/#!$%\^&\*;:{}=_`~()]/g, '')
+    .replace(/\s+/g, '-').toLowerCase();
+  res.redirect('/tag/' + search_term);
+});
+
+/* GET dashboard view for a tag search */
+router.get('/tag/:search_tag', function(req, res, next) {
+  var search_term = req.params.search_tag
+    .replace(/[\.,\/#!$%\^&\*;:{}=_`~()]/g, '')
+    .replace(/-/g, ' ').toLowerCase();
   var url = helpers.createRequestUrl(search_term, req.app.locals.config.api_key);
 
   request({url: url, json: true}, function(error, response, etsy_data) { 
@@ -18,11 +33,6 @@ router.post('/', function(req, res, next) {
       results: new Results(search_term, etsy_data.count, etsy_data.results) 
     });
   });
-});
-
-/* GET index view */
-router.get('/', function(req, res, next) {
-  res.render('index');
 });
 
 
