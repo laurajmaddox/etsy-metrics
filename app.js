28cab75ae99b67b78ff3ce342532908ce0523f98
Laura Maddox <ljgmaddox@gmail.com>
2015-03-08 14:14:16 -0700

    404 page + message for no results moved from pane to dashboard view

--- app.js
+++ app.js
@@ -53,10 +53,11 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    if (err.status === 404) {
+        res.render('404');
+    } else {
+        res.render('error');
+    }
 });
 
 
